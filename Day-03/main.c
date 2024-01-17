b58a97edef3ca075d411df071efc7e5e7ba94c83
DevilEyesMF <remkootje.claes@gmail.com>
2024-01-17 21:50:40 +0100

    Day 3, Part 2

--- Day-03/main.c
+++ Day-03/main.c
@@ -7,14 +7,14 @@
 #define INPUT_FILE "input.txt"
 #define MAX_COUNT 128
 #define ITEM_TYPES 52
+#define LINE_COUNT 3
 
 // function prototypes
+int partOne(FILE*);
+int partTwo(FILE*);
 char getItemType(char);
 
 int main() {
-    char rucksack[MAX_COUNT];
-    int result = 0;
-
     FILE* fstream = fopen(INPUT_FILE, "r");
 
     if (fstream == NULL) {
@@ -22,9 +22,23 @@ int main() {
         return -1;
     }
 
+    printf("The sum of the duplicate items is %d\n", partOne(fstream));
+    rewind(fstream);
+    printf("The sum of duplicate items in each Elf group is %d\n", partTwo(fstream));
+
+    fclose(fstream);
+
+    return 0;
+}
+
+int partOne(FILE* fstream) {
+    // local variables
+    char rucksack[MAX_COUNT];
+    int result = 0;
+
     while (fgets(rucksack, MAX_COUNT, fstream)) {
         int size, compartmentSize;
-        int map[ITEM_TYPES] = {0};
+        int map[ITEM_TYPES];
 
         // reset the map for each rucksack
         memset(map, 0, ITEM_TYPES * sizeof(int));
@@ -44,11 +58,49 @@ int main() {
             }
         }
     }
-    printf("The sum of the duplicate items is %d\n", result);
+    return result;
+}
 
-    fclose(fstream);
+int partTwo(FILE* fstream) {
+    // local variables
+    char rucksack[MAX_COUNT];
+    int map[ITEM_TYPES] = {0};
+    int result = 0;
 
-    return 0;
+    for (int lineCount = 0; fgets(rucksack, MAX_COUNT, fstream); lineCount++) {
+        int size = (int)strlen(rucksack) - 1;
+
+        switch(lineCount) {
+            // first rucksack
+            case 0:
+                for (int i = 0; i < size; i++) {
+                    map[getItemType(rucksack[i])] = 1;
+                }
+                break;
+            // last rucksack
+            case LINE_COUNT - 1:
+                for (int i = 0; i < size; i++) {
+                    if (map[getItemType(rucksack[i])] == lineCount) {
+                        result += getItemType(rucksack[i]) + 1;
+                        break;
+                    }
+                }
+                // reset map
+                memset(map, 0, ITEM_TYPES * sizeof(int));
+                // reset counter
+                lineCount = -1;
+                break;
+            // any other rucksack
+            default:
+                for (int i = 0; i < size; i++) {
+                    if (map[getItemType(rucksack[i])] == lineCount) {
+                        map[getItemType(rucksack[i])]++;
+                    }
+                }
+                break;
+        }
+    }
+    return result;
 }
 
 char getItemType(char item) {
